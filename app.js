86d3e0405b5449481d4d5542c34e34b081eba9a1
Aldin Wildan <08.aldinwildan@gmail.com>
2024-03-03 15:15:40 +0700

    menambah fitur divide

--- app.js
+++ app.js
@@ -7,5 +7,8 @@ console.log(add(1,2));
 const multiply = (a,b) => a*b;
 console.log(multiply(3,4));
 
+const divide = (a,b) => a/b;
+console.log(divide(8,4));
+
 // Ini adalah komentar satu baris
 // Ini adalah komentar dua baris
