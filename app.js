d6dc931e24164e789d33d33bf62b3779dd50549a
Aldin Wildan <08.aldinwildan@gmail.com>
2024-03-03 15:11:42 +0700

    menambah fitur pada branch berbeda

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ console.log('fungsi kedua');
 const add = (a,b) => a+b;
 console.log(add(1,2));
 
+const multiply = (a,b) => a*b;
+console.log(multiply(3,4));
+
 // Ini adalah komentar satu baris
 // Ini adalah komentar dua baris
